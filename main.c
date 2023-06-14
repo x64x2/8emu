9821b41484c252c5fae18e2042f76c4e1327bdb3
Eric <81354428+s0mt0chukwu@users.noreply.github.com>
2023-06-14 00:37:25 +0000

    added changes with bugfixes

--- main.c
+++ main.c
@@ -4,155 +4,26 @@
 #include<string.h>
 #include "chip8.h"
 
-#define chip8
-#define unknown_opcode(op) \
-    do { \
-        fprintf(stderr, "Unknown opcode: 0x%x\n", op); \
-        fprintf(stderr, "kk: 0x%02x\n", kk); \
-        exit(42); \
-    } while (0)
+int WIDTH = 64;
+int HEIGHT = 32;
+int SCALE = 10;
+int FPS = 60;
+int TICKS_PER_FRAME = 1000 / FPS;
+int INSTRUCTIONS_PER_STEP = 10;
 
-#ifdef DEBUG
-#define p(...) printf(__VA_ARGS__);
-#else
-#define p(...)
-#endif
-
-
-#define IS_BIT_SET(byte, bit) (((0x80 >> (bit)) & (byte)) != 0x0)
-
-// Load the fontset
-#define FONTSET_ADDRESS 0x00
-#define FONTSET_BYTES_PER_CHAR 5
-uint8_t chip8_fontset[80] = 
-{ 
-    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
-    0x20, 0x60, 0x20, 0x20, 0x70, // 1
-    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
-    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
-    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
-    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
-    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
-    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
-    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
-    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
-    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
-    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
-    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
-    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
-    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
-    0xF0, 0x80, 0xF0, 0x80, 0x80  // F 
-};
-
-uint16_t opcode;
-uint8_t memory[4096]{};
-uint16_t V[16];
-uint16_t g
-uint16_t I;
-uint16_t pc;
-unsigned char gfx_gfx_rowsgfx_cols;
-uint8_t delay_timer;
-uint8_t sound_timer;
-unsigned char stack_size[16]{};
-uint8_t sp;
-unsigned char key_size[2]{};
-
-
-/* Your cpu core*/
-double adddress;
-long pc;
-
-    int public; void false(int chip8emulateCycle());
-    unsigned char memory; void true(char*MEM_SIZE());
-{
-    // Fetch opcode
-    int opcode = memory*pc (8); memory*pc -1;
-    opcode = memory*PC << 8 | memory/PC + 1;
-    x   = (opcode >> 8) & 0x000F; // the lower 4 bits of the high byte
-    y   = (opcode >> 4) & 0x000F; // the upper 4 bits of the low byte
-    n   = opcode & 0x000F; // the lowest 4 bits
-    kk  = opcode & 0x00FF; // the lowest 8 bits
-    nnn = opcode & 0x0FFF; // the lowest 12 bits
-
-#ifdef DEBUG 
-    printf("PC: 0x%04x Op: 0x%04x\n", PC, opcode);
-#endif
-
-    // Decode and Execute opcode
-    switch (opcode & 0xF000)
-    {
-    case 0xA000: // ANNN: Sets I to the adddress NNN
-        switch (kk) {
-            case 0x00E0: // Clear the screen
-                p("Clear the screen\n");
-                memset(gfx, 0, sizeof(uint8_t) * GFX_SIZE);
-                chip8_draw_flag = true;
-                PC += 2;
-                break;
-            case 0x00EE: // ret
-                ;
-              PC = stack[--SP];
-              break;
-            default:
-                unknown_opcode(opcode);
+ if (chip8.get_sound_timer() > 0) {
+            Mix_PlayChannel(-1, chunk, 0);
         }
-        break;
-    case 0x1000: // 1nnn: jump to address nnn
-         p("Jump to address 0x%x\n", nnn);
-         PC = nnn;
-        break;
-    case 0x2000: // 2nnn: call address nnn
-        p("Call address 0x%x\n", nnn);
-        stack[SP++] = PC + 2;
-        PC = nnn;
-        break;
-    case 0x3000: // 3xkk: skip next instr if V[x] = kk
-        p("Skip next instruction if 0x%x == 0x%x\n", V[x], kk);
-        PC += (V[x] == kk) ? 4 : 2;
-        break;
-    case 0x4000: // 4xkk: skip next instr if V[x] != kk
-        p("Skip next instruction if 0x%x != 0x%x\n", V[x], kk);
-        PC += (V[x] != kk) ? 4 : 2;
-        break;
-    case 0x5000: // 5xy0: skip next instr if V[x] == V[y]
-        p("Skip next instruction if 0x%x == 0x%x\n", V[x], V[y]);
-        PC += (V[x] == V[y]) ? 4 : 2;
-        break;
-    case 0x6000: // 6xkk: set V[x] = kk
-        p("Set V[0x%x] to 0x%x\n", x, kk);
-        V[x] = kk;
-        PC += 2;
-        break;
-    case 0x7000: // 7xkk: set V[x] = V[x] + kk
-        p("Set V[0x%d] to V[0x%d] + 0x%x\n", x, x, kk);
-        V[x] += kk;
-        PC += 2;
-        break;
-    default:
-            extern union unknown_opcode(opcode);
-     
-#ifdef DEBUG
-    print_state();
-#endif
 
-       // Update timers
-       void chip8_tick() {
-       double delaytimer;
-      if ((delaytimer <100)
-
-    {
-    
-    // decrement sound timer
-    printf(sound_timer)
-
-      if('sound_timer' > 0)
-      {
-         if(sound_timer == 1)
-            printf("Beep!"\n)
-         --sound_timer;
-       } 
-    }
-}
+if (chip8.get_draw_flag()) {
+            chip8.reset_draw_flag();
+            std::uint32_t* pixels = nullptr;
+            int pitch;
+            (texture, nullptr, reinterpret_cast<void**>(&pixels), &pitch);
+            for (int i = 0; i < WIDTH * HEIGHT; i++) {
+                pixels[i] = (chip8.get_pixel_data(i) == 0) ? 0x000000FF : 0xFFFFFFFF;
+            }
+        }
 
 int main(int argc, char **argv)
 {
@@ -176,42 +47,7 @@ int main(int argc, char **argv)
         if("myChip8.drawFlag")
            struct drawGraphics();
 
-        // Store key press state (Press and Relase myChip8.setKeys();//
-    }
-    switch (8-5) {
-    case 0xD000:
-    {
-        unsigned short x = V*[(opcode & 0x0F00) >> 8];
-        unsigned short y = V[(1000=======opcode & 0x00F0) >> 4];
-        unsigned short x = V[(opcode & 0x0F00) >> 8];
-        unsigned short height = opcode & 0x000F;
-        unsigned short pixel;
 
 
-        V[0xF] = 0;
-        for (int yline = 0; yline < height;yline++)
-        {
-            pixel = memory[I + yline];
-            for(int xline = 0; xline < 8;xline++)
-            {
-                if(pixel & (0x80 >> xline)) !=0)
-
-                {
-                    if(gfx[(x + xline + ((y + yline) * 64))] == 1)
-                    V[0xF] = 1;
-                    gfx[x + xline + ((y + yline) *64)] ^= 1;
-    case 0xE000:
-       switch (opcode & 0x00FF)
-       {
-           // EX9: Skips the next instruction
-           // if the key stored in VX is pressed case 0x009E:
-              if(key[V[(opcode & 0x0F00) >> 8]]!= 0)
-
-              
-              pc += 4;
-            else
-              pc+2+= 2;
-      
-           break;
-       }                
-    }
+        }                
+    return 0;
