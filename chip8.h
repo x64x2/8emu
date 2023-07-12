d14cfa818f30e49e11b64367318667419628502f
Eric <81354428+s0mt0chukwu@users.noreply.github.com>
2023-07-12 01:57:27 +0000

    Add files via upload

--- chip8.h
+++ chip8.h
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cstdint>
 #include <string.h>
 
 #define true 1
@@ -15,6 +16,23 @@
 #define GFX_INDEX(row, col) ((row)*GFX_COLS + (col))
 
 #define MAX_GAME_SIZE (0x1000 - 0x200)
+
+int Chip8 {
+
+    std::array<uint8_t, 4096> memory;       // ram, first 512 bytes reserved
+    std::array<uint8_t, 16> V;              // general registers, VF = carry bit
+    std::array<uint16_t, 16> stack;         // subroutine return addresses
+    std::array<uint8_t, 16> keys;           // stores hexadecimal keypad
+    std::array<uint8_t, 64 * 32> graphics;  // holds pixel data
+    std::uint8_t delay_timer;               // decrements at 60Hz when nonzero
+    std::uint8_t sound_timer;               // decrements at 60Hz when nonzero
+    std::uint16_t I;                        // stores memory addresses
+    std::uint16_t pc;                       // currently executing address
+    std::uint16_t sp;                       // points to top of stack
+    std::uint16_t opcode;                   // current instruction
+    std::random_device rd;                  // used to obtain seed for generator
+    std::mt19937 gen;                       // generates pseudo-random numbers
+    bool draw_flag;                         // true when gfx needs to be updated
 
 const unsigned int START_ADDRESS = 0x200;
 
@@ -78,4 +96,5 @@ void Chip8::Cycle()
 	                  	--delayTimer;
 	            }
      break;
-} 
+};
+ 
